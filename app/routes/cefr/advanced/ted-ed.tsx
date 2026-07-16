f157c3611db6c111db43a26be63b9ceb057e542f
Paulo Silva <paulofernando78@gmail.com>
2026-07-16 04:22:23 -0300

    wip

--- app/routes/cefr/advanced/ted-ed.tsx
+++ app/routes/cefr/advanced/ted-ed.tsx
@@ -1,17 +1,17 @@
 import { Whiteboard } from "@/components/content/Whiteboard";
 import { LessonCard } from "@/components/content/LessonCard/LessonCard";
 
-import { lessons } from "@/data/cefr/advanced/ted-ed/ted-ed-lessons-card-data";
+import { advancedTedEdLessons } from "@/data/cefr/advanced/ted-ed/ted-ed-lessons-card-data";
 
 
 export default function Dashboard() {
   return (
     <>
       <Whiteboard title="Ted-Ed" subtitle="A variety of videos from Ted-Education Site" />
-      <LessonCard {...lessons.whatCausesConstipation} />
-      <LessonCard {...lessons.whenWillTheNextIceAgeHappen} />
-      <LessonCard {...lessons.howToManageEmotions} />
-      <LessonCard {...lessons.howDidAncientCivilizationsBrewBeer} />
+      <LessonCard {...advancedTedEdLessons.whatCausesConstipation} />
+      <LessonCard {...advancedTedEdLessons.whenWillTheNextIceAgeHappen} />
+      <LessonCard {...advancedTedEdLessons.howToManageEmotions} />
+      <LessonCard {...advancedTedEdLessons.howDidAncientCivilizationsBrewBeer} />
     </>
   );
 }
